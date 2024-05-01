3bc5699ec327aa912f852a6dbec0f642ebaa7235
CletsyMedia <Cletussam12@yahoo.com>
2024-05-01 03:54:31 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,53 +1,42 @@
 #!/usr/bin/python3
+"""A python module that determines if all boxes can be opened
+   from a list of lists
 """
-Determines if all boxes can be opened.
-"""
 
 
-def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
-
-    Args:
-        boxes (list of list): A list of lists representing the boxes and
-        their keys.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
+def canUnlockAll(boxes=[]):
+    """A function that returns True of all box in
+    boxes can be opend
     """
     if not boxes:
         return False
 
-    # Initialize a set to keep track of visited boxes
-    visited = set()
+    keys = set([0])
+    for box_id, box in enumerate(boxes):
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
 
-    # Initialize a stack with the first box
-    stack = [0]
+    if len(keys) != len(boxes):
+        return False
 
-    # Iterate over the stack until it's empty
-    while stack:
-        # Pop the last element from the stack
-        current_box = stack.pop()
-
-        # If the current box has not been visited yet
-        if current_box not in visited:
-            # Add it to the visited set
-            visited.add(current_box)
-
-            # Add all keys from the current box to the stack
-            for key in boxes[current_box]:
-                stack.append(key)
-
-    # If all boxes have been visited, return True
-    return len(visited) == len(boxes)
+    return True
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    boxes = [
+                [1, 3],
+                [2],
+                [3, 0],
+                [1, 2, 3],
+            ]
+    print(unlockBoxes(boxes))
+
     boxes = [[1], [2], [3], [4], []]
     print(canUnlockAll(boxes))
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
+    print(unlockBoxes(boxes))
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))
+    print(unlockBoxes(boxes))
