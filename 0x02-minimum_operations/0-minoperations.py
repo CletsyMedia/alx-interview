832e2e67a93c47b1324793851dc3d95969e337ce
CletsyMedia <Cletussam12@yahoo.com>
2024-05-08 22:41:02 +0100

    MinOps

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Minimum Operations: Given a number n, calculate the fewest number of
+operations needed to result in exactly n H characters in a text file."""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H
+    characters in the file.
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The fewest number of operations needed.
+
+    Raises:
+        TypeError: If n is not an integer.
+
+    Notes:
+        If n is impossible to achieve, returns 0.
+    """
+    if not isinstance(n, int):
+        raise TypeError("n must be an integer")
+
+    operations = 0
+    for iterator in range(2, n + 1):
+        while (n % iterator) == 0:
+            n //= iterator
+            operations += iterator
+    return operations
